f8b155d1d1587e767dd5e7298f301e610a166001
raboniribeiro-ops <raboniribeiro@gmail.com>
2026-06-25 01:39:25 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter_Tight } from "next/font/google";
 import "./globals.css";
 
 const interTight = Inter_Tight({
-  variable: "--font-inter-tight",
+  variable: "--font-work-sans",
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   style: ["normal", "italic"],
